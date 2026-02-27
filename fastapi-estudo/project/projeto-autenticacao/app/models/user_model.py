7e354a03155d32aa2d22b6502893b71b7b60d913
thales willian <dtwillian21@gmail.com>
2026-02-27 09:41:01 -0300

    feito

--- fastapi-estudo/project/projeto-autenticacao/app/models/user_model.py
+++ fastapi-estudo/project/projeto-autenticacao/app/models/user_model.py
@@ -2,13 +2,13 @@ from beanie import Document, Indexed
 from uuid import UUID, uuid4
 from pydantic import Field, EmailStr
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Annotated
 
 class User(Document):
     user_id: UUID = Field(default_factory=uuid4)
-    username: Indexed(str, unique=True)
-    email: Indexed(EmailStr, unique=True)
-    hash_passowrd: str
+    username: Annotated[str, Indexed(unique=True)]
+    email: Annotated[EmailStr,Indexed(unique=True)]
+    hashed_password: str
     first_name: Optional[str] = None
     last_name:  Optional[str] = None
     disable: Optional[bool] = None
@@ -19,3 +19,18 @@ class User(Document):
     def __str__(self) -> str:
         return self.email
     
+    def __hash__(self) -> str:
+        return hash(self.email)
+    
+    def __eq__ (self, other: object) -> bool:
+        if isinstance(other, User):
+            return self.email == other.email
+        return False
+    
+    @property
+    def create(self) -> datetime:
+        return self.id.generation_time
+    
+    @classmethod
+    async def by_email(self, email: str) -> "User":
+        return await self.find_one(self.email == email)
